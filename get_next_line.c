47f4330ae28203c64da566b50ae639904158ab4f
Hugo MARIE <hmarie@e2r8p15.42.fr>
2013-12-12 13:49:27 +0100

    push gnl

--- get_next_line.c
+++ get_next_line.c
@@ -5,56 +5,139 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmarie <hmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/12/03 12:37:23 by hmarie            #+#    #+#             */
-/*   Updated: 2013/12/05 16:53:20 by hmarie           ###   ########.fr       */
+/*   Created: 2013/12/06 10:34:23 by hmarie            #+#    #+#             */
+/*   Updated: 2013/12/12 11:18:35 by hmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "get_next_line.h"
 
-int	get_next_line(int const fd, char ** line)
+static char	*ft_concat(t_list **lst, t_list **tmp)
 {
-	int					ret;
- 	char				*buf[BUF_SIZE + 1];
-	static t_list		*lst;
-	t_list				*ptr;
-	int					i;
-	char				*str;
-	char				*str2;
-	void				(*ft)(void *, size_t);
-
-	i = 1;
-	ft = ft_lstmdel;
-	lst = ft_lstnew(buf, BUF_SIZE + 1);
-	while ((ret = read(fd, buf, BUF_SIZE)))
+	char	*s;
+	s = ft_strnew(0);
+	ft_putendl("TEST 10");
+	ft_putendl((*lst)->content);
+	*lst = *tmp;
+	while (*lst)
 	{
-		if (ret == -1)
-			return (-1);
-		buf[ret] = 0;
-		if (ft_memchr(buf, '\n', BUF_SIZE) == NULL)
-		{
-			lst->next = ft_lstnew(buf, BUF_SIZE + 1);
-			i++;
-		}
-		else
-			break ;
+		ft_putendl("TEST 11");
+		ft_putstr("CONTENT AVANT: ");
+		ft_putendl((*lst)->content);
+		ft_putstr("CONTENU DE S AVANT: ");
+		ft_putendl(s);
+		s = ft_strjoin(s, (*lst)->content);
+		ft_putstr("CONTENT APRES: ");
+		ft_putendl((*lst)->content);
+		ft_putstr("CONTENU DE S APRES: ");
+		ft_putendl(s);
+		*lst = (*lst)->next;
 	}
-	if ((str = (char *)malloc(sizeof(str) * (BUF_SIZE * i)  + 1)) == NULL)
-		return (0);
-	while (lst)
-	{
-		str = ft_strjoin(str, lst->content);
-		ptr = lst->next;
-		ft_lstdelone(&lst, ft);
-		lst = ptr;
-	}
-	i = 0;
-	while (str[i] != '\n')
-		i++;
-	while (str[i])
-		str[i] = 0;
-	lst->content = ft_strcpy(ft_strnew(ft_memlen(lst->content, '\n', BUF_SIZE)), ft_strchr(lst->content, '\n'));
-	*line = *str;
-	return (0);
+	return (s);
 }
 
+static void	ft_newlst(char **buf, t_list **lst, t_list **tmp)
+{
+	ft_putendl("TEST 7");
+	if (*lst)
+	{
+		ft_putendl("TEST 9");
+		*lst = ft_lstnewend(*buf, ft_strlen(*buf), *lst);
+		ft_putendl((*lst)->content);
+		ft_putendl("TEST 9-");
+	}
+	else
+	{
+		ft_putendl("TEST 8");
+		*lst = ft_lstnew(*buf, ft_strlen(*buf));
+		*tmp = *lst;
+		ft_putendl((*lst)->content);
+		ft_putendl("TEST 8-");
+	}
+}
+
+static int	ft_check_buf(char **line, char **save, char *buf, t_list **lst, t_list **tmp)
+{
+	int	i;
+	ft_putendl("TEST 5");
+
+	if ((i = ft_strichr(buf, '\n')) >= 0)
+	{
+		ft_putendl("TEST 6");
+		ft_putnbr(i);
+		ft_strncpy(*line, buf, i);
+		ft_putendl(*line);
+		**(line + i) = 0;
+		ft_putendl(*line);
+		ft_newlst(line, lst, tmp);
+		*save = ft_strsub(buf, i + 1, (BUF_SIZE - i));
+		ft_putendl(*save);
+		*line = ft_concat(lst, tmp);
+		return (1);
+	}
+	else
+	{
+		ft_putendl("TEST 6.5");
+		ft_newlst(&buf, lst, tmp);
+		return (0);
+	}
+}
+
+static int	ft_check_save(char **line, char **save, t_list **lst, t_list **tmp)
+{
+	int	i;
+
+	if ((i = ft_strichr(*save, '\n')) >= 0)
+	{
+		ft_putendl("TEST 12");
+		ft_strncpy(*line, *save, i);
+		*(line + i) = 0;
+		*save = ft_strsub(*save, i + 1, ft_strlen(*save));
+		return (1);
+	}
+	else
+	{
+	ft_putendl("TEST 13");
+		*lst = ft_lstnew(*save, ft_strlen(*save));
+	ft_putendl("TEST 14");
+		*tmp = *lst;
+		ft_putendl(*save);
+	ft_putendl("TEST 15");
+		free (*save);
+	ft_putendl("TEST 16");
+		*save = NULL;
+	ft_putendl("TEST 17");
+		return (0);
+	}
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	static char	*save = NULL;
+	t_list		*lst;
+	t_list		*tmp;
+	char		*buf;
+
+	*line = ft_strnew(0);
+	buf = ft_strnew(BUF_SIZE + 1);
+	lst = NULL;
+	tmp = NULL;
+	ft_putendl("TEST 1");
+	if (save)
+	{
+		ft_putendl("TEST 2");
+		if (ft_check_save(line, &save, &lst, &tmp) == 1)
+		{
+			return (1);
+		}
+	}
+	ft_putendl("TEST 2.5");
+	while (read(fd, buf, BUF_SIZE) > 0)
+	{
+		ft_putendl("TEST 4");
+		if (ft_check_buf(line, &save, buf, &lst, &tmp) == 1)
+			return (1);
+	}
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,18 @@
 /*   By: hmarie <hmarie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/03 14:25:27 by hmarie            #+#    #+#             */
-/*   Updated: 2013/12/05 12:51:17 by hmarie           ###   ########.fr       */
+/*   Updated: 2013/12/12 11:19:43 by hmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __GET_NEXT_LINE_H__
 # define __GET_NEXT_LINE_H__
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# define BUF_SIZE 3
 
 int	get_next_line(int const ft, char ** line);
 
